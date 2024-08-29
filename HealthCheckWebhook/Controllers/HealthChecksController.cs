225fffb4633041cb483718e4c13b6c01b0f49b93
limon-7 <liibd7@gmail.com>
2024-08-29 10:21:51 +0600

    Feat: add initial project for webhook and helthcheck

---
+++ HealthCheckWebhook/Controllers/HealthChecksController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthCheckWebhook.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthChecksController : ControllerBase
+{
+    private readonly ILogger<HealthChecksController> _logger;
+
+    public HealthChecksController(ILogger<HealthChecksController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetHealthChecks()
+    {
+        var response= await Task.FromResult(0);
+       return Ok(new {Statue=true});
+    }
+}
