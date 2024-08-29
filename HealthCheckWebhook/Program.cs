120d902800e5a3e96bf168be8ecf04dedf455f38
limon-7 <liibd7@gmail.com>
2024-08-29 10:44:50 +0600

    Feat: add redis healthcheck

--- HealthCheckWebhook/Program.cs
+++ HealthCheckWebhook/Program.cs
@@ -1,3 +1,7 @@
+using HealthChecks.UI.Client;
+using HealthCheckWebhook.Infrastructure;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddInfrastructure(builder.Configuration);
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
@@ -21,5 +27,14 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.UseRouting()
+    .UseEndpoints(config =>
+    {
+        config.MapHealthChecks("/healthz", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+    });
 
 app.Run();
